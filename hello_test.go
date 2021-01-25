15787788e109bd111b2109df1d63b1a32823633e
Chris H <redpandaxlbusiness@gmail.com>
2021-01-24 20:30:07 -0800

    running test

--- hello_test.go
+++ hello_test.go
@@ -3,8 +3,8 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Chris")
-	want := "Hello, Chris"
+	got := Hello("Chris2")
+	want := "Hello, Chris2"
 
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
